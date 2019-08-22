64b27a330458ecf98c3a2953d5c8fee879d336ba
leo <leobin001@163.com>
2019-08-22 22:58:46 +0800

    集成redis(finished)

--- src/main/java/com/imooc/miaosha/controller/SampleController.java
+++ src/main/java/com/imooc/miaosha/controller/SampleController.java
@@ -2,6 +2,7 @@ package com.imooc.miaosha.controller;
 
 import com.imooc.miaosha.domain.User;
 import com.imooc.miaosha.redis.RedisService;
+import com.imooc.miaosha.redis.UserKey;
 import com.imooc.miaosha.result.Result;
 import com.imooc.miaosha.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,9 +43,20 @@ public class SampleController {
 
     @RequestMapping("/redis/get")
     @ResponseBody
-    public Result<Long> redisGet() {
-        Long v1 = redisService.get("key1", Long.class);
-        return Result.success(v1);
+    public Result<User> redisGet() {
+        User user = redisService.get(UserKey.getById, ""+1, User.class);
+        return Result.success(user);
+    }
+
+    @RequestMapping("/redis/set")
+    @ResponseBody
+    public Result<Boolean> redisSet() {
+        User user = new User();
+        user.setId(1);
+        user.setName("1111");
+        redisService.set(UserKey.getById, ""+1, user);//UserKey:id1
+
+        return Result.success(true);
     }
 
 
