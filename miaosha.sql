6f8f7d69184ec965036b36fb14c3193feb015777
leo <leobin001@163.com>
2019-08-23 23:06:44 +0800

    sql

--- miaosha.sql
+++ miaosha.sql
@@ -10,10 +10,67 @@ Target Server Type    : MYSQL
 Target Server Version : 50547
 File Encoding         : 65001
 
-Date: 2019-08-23 19:45:45
+Date: 2019-08-23 23:06:18
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for goods
+-- ----------------------------
+DROP TABLE IF EXISTS `goods`;
+CREATE TABLE `goods` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '商品ID',
+  `goods_name` varchar(16) DEFAULT NULL COMMENT '商品名称',
+  `goods_title` varchar(64) DEFAULT NULL COMMENT '商品标题',
+  `goods_img` varchar(64) DEFAULT NULL COMMENT '商品的图片',
+  `goods_detail` longtext COMMENT '商品的详情介绍',
+  `goods_price` decimal(10,2) DEFAULT '0.00' COMMENT '商品单价',
+  `goods_stock` int(11) DEFAULT '0' COMMENT '商品库存，-1表示没有限制',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of goods
+-- ----------------------------
+INSERT INTO `goods` VALUES ('1', 'iphoneX', 'Apple iPhone X(A1865) 64G', '/img/iphonex.png', 'pple iPhone X(A1865) 64G 银色 三网通', '8765.00', '10000');
+INSERT INTO `goods` VALUES ('2', '华为Meta9', '华为Mate9 4GB+32GB版 月光银', '/img/meta10.png', '华为Mate9 4GB+32GB版 月光银 三网通', '3212.00', '-1');
+
+-- ----------------------------
+-- Table structure for miaosha_goods
+-- ----------------------------
+DROP TABLE IF EXISTS `miaosha_goods`;
+CREATE TABLE `miaosha_goods` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '秒杀的商品表',
+  `goods_id` bigint(20) DEFAULT NULL COMMENT '商品ID',
+  `miaosha_price` decimal(10,2) DEFAULT '0.00' COMMENT '秒杀价',
+  `stock_count` int(11) DEFAULT NULL COMMENT '库存数量',
+  `start_date` datetime DEFAULT NULL COMMENT '秒杀开始时间',
+  `end_date` datetime DEFAULT NULL COMMENT '秒杀介绍时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of miaosha_goods
+-- ----------------------------
+INSERT INTO `miaosha_goods` VALUES ('1', '1', '0.01', '4', '2017-11-05 15:18:00', '2017-11-13 14:18:00');
+INSERT INTO `miaosha_goods` VALUES ('2', '2', '0.01', '9', '2017-11-12 14:00:10', '2017-11-13 14:00:00');
+
+-- ----------------------------
+-- Table structure for miaosha_order
+-- ----------------------------
+DROP TABLE IF EXISTS `miaosha_order`;
+CREATE TABLE `miaosha_order` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '秒杀订单Id',
+  `user_id` bigint(20) DEFAULT NULL COMMENT '用户ID',
+  `order_id` bigint(20) DEFAULT NULL COMMENT '订单ID',
+  `goods_id` bigint(20) DEFAULT NULL COMMENT '商品ID',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of miaosha_order
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for miaosha_user
@@ -35,6 +92,29 @@ CREATE TABLE `miaosha_user` (
 -- Records of miaosha_user
 -- ----------------------------
 INSERT INTO `miaosha_user` VALUES ('18912341234', 'Joshua', 'b7797cce01b4b131b433b6acf4add449', '1a2b3c4d', null, '2019-08-23 19:33:30', '2019-08-23 19:33:33', '0');
+
+-- ----------------------------
+-- Table structure for order_info
+-- ----------------------------
+DROP TABLE IF EXISTS `order_info`;
+CREATE TABLE `order_info` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '订单Id',
+  `user_id` bigint(20) DEFAULT NULL COMMENT '用户ID',
+  `goods_id` bigint(20) DEFAULT NULL COMMENT '商品ID',
+  `delivery_addr_id` bigint(20) DEFAULT NULL COMMENT '收货地址ID',
+  `goods_name` varchar(16) DEFAULT NULL COMMENT '沉余过来的商品名称',
+  `goods_count` int(11) DEFAULT '0' COMMENT '商品数量',
+  `goods_price` decimal(10,2) DEFAULT '0.00' COMMENT '商品单价',
+  `order_channel` tinyint(4) DEFAULT '0' COMMENT '1pc, 2android, 3ios',
+  `status` tinyint(4) DEFAULT '0' COMMENT '订单状态,0新建未支付，1已支付，2已发货，3已收货，4已退款，5已完成',
+  `create_date` datetime DEFAULT NULL COMMENT '订单创建时间',
+  `pay_date` datetime DEFAULT NULL COMMENT '支付时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of order_info
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for user
