76c1c8ce879f868422efb3c262db621f65f4df38
leo <leobin001@163.com>
2019-08-24 23:28:09 +0800

    打包配置

--- src/main/java/com/imooc/miaosha/MainApplication.java
+++ src/main/java/com/imooc/miaosha/MainApplication.java
@@ -4,9 +4,19 @@ package com.imooc.miaosha;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//打包用
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//importorg.springframework.boot.web.support.SpringBootServletInitializer;
+
 @SpringBootApplication
-public class MainApplication {
+public class MainApplication /*extends SpringBootServletInitializer打包用*/{
     public static void main(String[] args) throws Exception{
         SpringApplication.run(MainApplication.class, args);
     }
+
+    //打包用
+    /*@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MainApplication.class);
+    }*/
 }
